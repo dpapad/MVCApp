1dd90545455528e29511692c44af254a9de5b82a
dpap@outlook.com <>
2015-07-24 18:49:51 +0300

    Inserted H2Database dependency in POM. Created H2SqlConnectionProvider. Created JUnit test.

---
+++ src/test/java/org/mypackage/dal/mysql/H2SqlConnectionProvider.java
@@ -0,0 +1,31 @@
+package org.mypackage.dal.mysql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import org.mypackage.dal.sql.SqlConnectionProvider;
+
+/**
+ *
+ * @author dev-dp
+ */
+
+
+// DONE
+
+public class H2SqlConnectionProvider implements SqlConnectionProvider {
+    
+    private static final String H2DB_DRIVER = "org.h2.driver";
+    private static final String H2DB_CONNECTION = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
+    private static final String H2DB_USERNAME = "";
+    private static final String H2DB_PASSWORD = "";
+
+    @Override
+    public Connection createConnection() throws ClassNotFoundException, SQLException {
+        //Class.forName(H2DB_DRIVER);
+        
+        return DriverManager.getConnection(H2DB_CONNECTION, H2DB_USERNAME, H2DB_PASSWORD);
+    }
+    
+    
+}
---
+++ src/test/java/org/mypackage/dal/mysql/MySqlContactRepositoryTest.java
@@ -0,0 +1,203 @@
+package org.mypackage.dal.mysql;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.mypackage.dal.sql.SqlConnectionProvider;
+import org.mypackage.model.Contact;
+
+/**
+ *
+ * @author dev-dp
+ */
+public class MySqlContactRepositoryTest {
+    
+    private SqlConnectionProvider connectionProvider;
+    private MysqlContactRepository contactRepository;
+    
+    public MySqlContactRepositoryTest() {
+        this.connectionProvider = new H2SqlConnectionProvider();
+        this.contactRepository = new MysqlContactRepository(this.connectionProvider);
+    }
+    
+    @Before
+    public void setUp() {
+        Connection connection = null;
+        
+        PreparedStatement createDatabaseStmt = null;
+        PreparedStatement createContactTableStmt = null;
+        
+        try {
+           try {
+               connection = this.connectionProvider.createConnection();
+               connection.setAutoCommit(false);
+               try {
+                   
+//                   try {
+//                       createDatabaseStmt = connection.prepareStatement("create database mvcproject;");
+//                       createDatabaseStmt.execute();
+//                   }
+//                   finally {
+//                       if(createDatabaseStmt!=null) {
+//                           createDatabaseStmt.close();
+//                       }
+//                   }
+//                   
+                   try {
+                   createContactTableStmt = connection.prepareStatement("CREATE TABLE Contact (" + 
+                                                                       "Id INT primary key auto_increment, " + 
+                                                                       "FullName VARCHAR(45), " + 
+                                                                       "Nickname VARCHAR(45), " + 
+                                                                       "Notes VARCHAR(45) " + 
+                                                                   ");");
+                   createContactTableStmt.execute();
+                   } 
+                   finally {
+                       if(createContactTableStmt!=null) {
+                           createContactTableStmt.close();
+                       }
+                   }
+               }
+               finally {
+                   connection.setAutoCommit(true);
+               }
+               
+           }
+           finally {
+               if(connection != null) {
+                   connection.close();
+               }
+           }
+        } 
+        catch (Exception ex){
+        }
+    }
+    
+    @After
+    public void tearDown() {
+        Connection connection = null;
+        
+        PreparedStatement dropDatabaseStmt = null;
+        
+        try {
+            
+            try {
+                
+                connection = this.connectionProvider.createConnection();
+                connection.setAutoCommit(false);
+               
+                try {
+                    dropDatabaseStmt = connection.prepareStatement("drop database mvcproject;");
+                }
+                finally {
+                    if(dropDatabaseStmt!=null) {
+                        dropDatabaseStmt.close();
+                    }
+                }
+            }
+            finally {
+                if(connection != null) {
+                    connection.close();
+                }
+            }
+        } 
+        catch (Exception ex){
+        }
+    }
+    
+    @Test
+    public void testConnection() throws Exception{
+        assertNotNull(connectionProvider.createConnection());
+    }
+    
+    @Test 
+    public void testAddContact() throws Exception {
+        Contact c = new Contact();
+        c.setFullName("xxx");
+        c.setNickname("xxx");
+        c.setNotes("xxx");
+        this.contactRepository.addContact(c);
+        int contactId = this.getMaxContactId();
+        Contact c2 = this.contactRepository.getContactById(contactId);
+        
+        assertEquals(c.getFullName(), c2.getFullName());
+        assertEquals(c.getNickname(), c2.getNickname());
+        assertEquals(c.getNotes(), c2.getNotes());
+    }
+    
+    @Test 
+    public void testAddContact2() throws Exception {
+        Contact c = new Contact();
+        c.setFullName("yyy");
+        c.setNickname("yyy");
+        c.setNotes("yyy");
+        this.contactRepository.addContact(c);
+        int count = this.getContactCount();
+        
+        assertEquals(1, count);
+    }
+    
+    private int getMaxContactId() throws Exception {
+        Connection connection = null;
+        PreparedStatement getMaxContactIdStmt = null;
+        int maxContactId;
+        
+        try{
+                connection = this.connectionProvider.createConnection();
+                try {
+                   getMaxContactIdStmt = connection.prepareStatement("select max(Id) from Contact");
+                   ResultSet rs = getMaxContactIdStmt.executeQuery();
+                   
+                   rs.next();
+                   
+                   maxContactId = rs.getInt(1);
+                   
+                }
+                finally {
+                    if(getMaxContactIdStmt != null){
+                        getMaxContactIdStmt.close();
+                    }
+                }
+            } 
+            finally {
+                if(connection != null) {
+                    connection.close();
+                }
+            }
+        return maxContactId;
+    }
+    
+    private int getContactCount() throws Exception {
+        Connection connection = null;
+        PreparedStatement getContactCountStmt = null;
+        int contactCount;
+        
+        try {
+            connection = this.connectionProvider.createConnection();
+            try{
+                getContactCountStmt = connection.prepareStatement("select count(*) from Contact");
+                ResultSet rs = getContactCountStmt.executeQuery();
+                
+                rs.next();
+                contactCount = rs.getInt(1);
+                
+            }
+            finally {
+                if(getContactCountStmt != null) {
+                    getContactCountStmt.close();
+                }
+            }
+        }
+        finally {
+            if(connection != null) {
+                connection.close();
+            }
+        }
+        
+        return contactCount;
+    }
+}
