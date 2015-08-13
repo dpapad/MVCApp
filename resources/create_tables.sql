acfd91200085932879b0e023b82dad13c6d17c1b
Dimitris Papadimitriou <dpap@outlook.com>
2015-08-13 16:15:03 +0300

    Added sql commands generated from Workbench

--- resources/create_tables.sql
+++ resources/create_tables.sql
@@ -13,3 +13,23 @@ CREATE TABLE ContactEmails (
     FOREIGN KEY (fContactId) REFERENCES Contact (Id) ON UPDATE CASCADE
 );
 
+
+CREATE TABLE `Contact` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `FullName` varchar(45) DEFAULT NULL,
+  `Nickname` varchar(45) DEFAULT NULL,
+  `Notes` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `Emails` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Address` varchar(50) DEFAULT NULL,
+  `Category` tinyint(4) DEFAULT NULL,
+  `fContactId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `Address` (`Address`),
+  KEY `fContactId` (`fContactId`),
+  CONSTRAINT `Emails_ibfk_1` FOREIGN KEY (`fContactId`) REFERENCES `Contact` (`Id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=latin1;
+
