1a619649b70a75f0927c42139838b0b5cc0edcc5
Dimitris Papadimitriou <dpap@outlook.com>
2015-08-24 15:09:29 +0300

    Commented the whole class for safety purposes. Will be removed completely soon.

--- src/main/java/org/mypackage/application/ApplicationDependencies.java
+++ src/main/java/org/mypackage/application/ApplicationDependencies.java
@@ -1,25 +1,25 @@
-package org.mypackage.application;
-
-import org.mypackage.controller.ControllerFactory;
-import org.mypackage.dal.RepositoryFactory;
-import org.mypackage.dal.mysql.MySqlRepositoryFactory;
-import org.mypackage.controller.impl.ControllerFactoryImpl;
-
-/**
- *
- * @author dpa
- */
-public final class ApplicationDependencies {
-    
-    public static final RepositoryFactory REPOSITORY_FACTORY;
-    public static final ControllerFactory CONTROLLER_FACTORY;
-    static {
-        REPOSITORY_FACTORY = new MySqlRepositoryFactory();
-    }
-    
-    static {
-        CONTROLLER_FACTORY = new ControllerFactoryImpl();
-    }   
-    
-    private ApplicationDependencies() { }
-}
+//package org.mypackage.application;
+//
+//import org.mypackage.controller.ControllerFactory;
+//import org.mypackage.dal.RepositoryFactory;
+//import org.mypackage.dal.mysql.MySqlRepositoryFactory;
+//import org.mypackage.controller.impl.ControllerFactoryImpl;
+//
+///**
+// *
+// * @author dpa
+// */
+//public final class ApplicationDependencies {
+//    
+//    public static final RepositoryFactory REPOSITORY_FACTORY;
+//    public static final ControllerFactory CONTROLLER_FACTORY;
+//    static {
+//        REPOSITORY_FACTORY = new MySqlRepositoryFactory();
+//    }
+//    
+//    static {
+//        CONTROLLER_FACTORY = new ControllerFactoryImpl();
+//    }   
+//    
+//    private ApplicationDependencies() { }
+//}
