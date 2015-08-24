eddd4fb1dd83549ffb7253dbcc5dcaf7c688681d
Dimitris Papadimitriou <dpap@outlook.com>
2015-08-24 10:15:57 +0300

    Removed unnecessary import

--- src/test/java/org/mypackage/dal/mysql/MySqlContactRepositoryTest.java
+++ src/test/java/org/mypackage/dal/mysql/MySqlContactRepositoryTest.java
@@ -1,8 +1,5 @@
 package org.mypackage.dal.mysql;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -20,7 +17,6 @@ import org.mypackage.model.Email;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
