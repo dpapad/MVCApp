62a05bae325347781d96266ad55bb4c88dcaa899
Dimitris Papadimitriou <dpap@outlook.com>
2015-09-02 11:43:35 +0300

    Added handlers for updating Emails in EmailsSpringController

--- src/main/java/org/mypackage/spring/controllers/EmailsSpringController.java
+++ src/main/java/org/mypackage/spring/controllers/EmailsSpringController.java
@@ -5,13 +5,16 @@
  */
 package org.mypackage.spring.controllers;
 
+import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
 import org.mypackage.application.errors.DuplicateEmailException;
 import org.mypackage.application.errors.MalformedCategoryException;
 import org.mypackage.application.errors.MalformedIdentifierException;
+import org.mypackage.controller.ContactsController;
 import org.mypackage.controller.DeleteEmailController;
 import org.mypackage.controller.NewEmailController;
+import org.mypackage.controller.UpdateEmailController;
 import org.mypackage.dal.DalException;
 import org.mypackage.model.Email;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +42,10 @@ public class EmailsSpringController {
     NewEmailController newEmailController;
     @Autowired
     DeleteEmailController deleteEmailController;
+    @Autowired
+    UpdateEmailController updateEmailController;
+    @Autowired
+    ContactsController contactsController;
 
     // [handler] GET method for requests about creating a new mail 
     @RequestMapping(value = "/contacts/{id}/new_email", method = RequestMethod.GET)
@@ -51,17 +58,14 @@ public class EmailsSpringController {
     }
 
     // [handler] POST method for requests about creating a new mail
-    @RequestMapping(value = "/contacts/{id}/new_email_submitted", method = RequestMethod.POST)
+    @RequestMapping(value = "/contacts/{id}/modify/new_email_submitted", method = RequestMethod.POST)
     public ModelAndView postCreateNewEmail(@PathVariable String id, @RequestParam("address") String address, @RequestParam("category") String categoryValue, @RequestParam("contactId") String contactId) {
         ModelAndView modelAndView = new ModelAndView();
-//        String state = "false";
         try {
             int newEmailId = newEmailController.addNewEmail(address, categoryValue, id);
-//            state = "true";
             modelAndView.addObject("emailId", newEmailId);
-//            modelAndView.addObject("state", state);
             modelAndView = contactsSpringController.getAContact(id);
-            
+            modelAndView.setViewName("redirect:/contacts/" + id + "/modify");
         } catch (DalException ex) {
             logger.error("An error occured while trying to add a new email for contact with ID = " + id
                     + "Email object parameters: "
@@ -80,7 +84,6 @@ public class EmailsSpringController {
             modelAndView.addObject("errorMessage", "An internal conflict concerning the category of the email occured. Please try again.");
             modelAndView.setViewName("/errorPage.jsp");
         } catch (DuplicateEmailException ex) {
-//            modelAndView.addObject("state", state);
             modelAndView = getCreateNewEmail(id);
             modelAndView.addObject("errorMessage", "This address already exists. Please enter a new one.");
         }
@@ -88,14 +91,13 @@ public class EmailsSpringController {
     }
 
     // [handler] GET method for deleting emails with specific id (one at a time)
-    @RequestMapping(value = "/contacts/{id}/delete_email/{emailId}", method = RequestMethod.GET)
+    @RequestMapping(value = "/contacts/{id}/modify/delete_email/{emailId}", method = RequestMethod.GET)
     public ModelAndView getDeleteEmail(@PathVariable String id, @PathVariable String emailId) {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject(id);
-        modelAndView.setViewName("redirect:/contacts/" + id);
         try {
             this.deleteEmailController.deleteEmail(emailId);
-            modelAndView.setViewName("/contacts/" + id);
+            modelAndView.setViewName("/contacts/" + id + "/modify");
         } catch (DalException ex) {
             logger.error("A database error occured while trying to delete contact with ID = " + id, ex);
             modelAndView.addObject("errorCode", HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
@@ -108,4 +110,68 @@ public class EmailsSpringController {
         }
         return modelAndView;
     }
+
+    // UPDATE Contact Handlers
+    // [handler] GET method used to retrieve a contact to be updated according to its ID
+    @RequestMapping(value = "/contacts/{id}/modify/update_email/{emailId}", method = RequestMethod.GET)
+    public ModelAndView getUpdateEmail(@PathVariable String id, @PathVariable String emailId) {
+        ModelAndView modelAndView = new ModelAndView();
+        try {
+            Email email = updateEmailController.retrieveEmail(emailId);
+            modelAndView.addObject(id);
+            modelAndView.addObject("emailId", email.getId());
+            modelAndView.addObject("email", email);
+            modelAndView.addObject("mailCategories", Email.Category.values());
+            modelAndView.setViewName("/updateEmail.jsp");
+        } catch (DalException ex) {
+            logger.error("A database error occured while trying to retrieve contact with ID = " + id, ex);
+            modelAndView.addObject("errorCode", HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            modelAndView.addObject("errorMessage", "There was a an internal database error.");
+            modelAndView.setViewName("errorPage.jsp");
+        } catch (MalformedIdentifierException ex) {
+            modelAndView.addObject("errorCode", HttpServletResponse.SC_BAD_REQUEST);
+            modelAndView.addObject("errorMessage", "An error occured because of a malformed id (caused by id = " + id + "). Please use only numeric values.");
+            modelAndView.setViewName("/errorPage.jsp");
+        }
+        return modelAndView;
+    }
+
+    // [handler] POST method for updating email with specific id (one at a time)
+    /**
+     *
+     * @param id
+     * @param emailId
+     * @param address
+     * @param categoryValue
+     * @return
+     */
+    @RequestMapping(value = "/contacts/{id}/modify/update_email/{emailId}/email_updated", method = RequestMethod.POST)
+    public ModelAndView postUpdateEmail(@PathVariable String id, @PathVariable String emailId, @RequestParam("address") String address, @RequestParam("category") String categoryValue) {
+        ModelAndView modelAndView = new ModelAndView();
+        try {
+            updateEmailController.updateEmail(emailId, address, categoryValue, id);
+            List<Email> emailsList = contactsController.retrieveAllEmails(id);
+            modelAndView.addObject("emailList", emailsList);
+            modelAndView.addObject("contactId", id);
+            modelAndView.setViewName("redirect:/contacts/" + id + "/modify");
+        } catch (DalException ex) {
+            logger.error("A database error occured while trying to delete contact with ID = " + id, ex);
+            modelAndView.addObject("errorCode", HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            modelAndView.addObject("errorMessage", "There was a an internal database error.");
+            modelAndView.setViewName("/errorPage.jsp");
+        } catch (MalformedIdentifierException ex) {
+            modelAndView.addObject("errorCode", HttpServletResponse.SC_BAD_REQUEST);
+            modelAndView.addObject("errorMessage", "An error occured because of a malformed id." + id + " Please use only numeric values.");
+            modelAndView.setViewName("/errorPage.jsp");
+        } catch (MalformedCategoryException ex) {
+            modelAndView.addObject("errorCode", HttpServletResponse.SC_CONFLICT);
+            modelAndView.addObject("errorMessage", "An internal conflict concerning the category of the email occured. Please try again.");
+            modelAndView.setViewName("/errorPage.jsp");
+        } catch (DuplicateEmailException ex) {
+            modelAndView = getUpdateEmail(id, emailId);
+            modelAndView.addObject("errorMessage", "The address you tried to submit already exists. Please enter a different one.");
+        }
+        return modelAndView;
+    }
+
 }
